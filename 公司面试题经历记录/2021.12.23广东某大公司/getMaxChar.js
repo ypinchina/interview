387460efb809529bca8e3a2f192d53edb2282485
ypinchina <batmanyp@foxmail.com>
2021-12-23 18:43:05 +0800

    更新面试记录

---
+++ 公司面试题经历记录/2021.12.23广东某大公司/getMaxChar.js
@@ -0,0 +1,22 @@
+// 求一个字符串中出现最多次数的字符，并返回他的次数
+function getMaxChar(str) {
+  const map = new Map()
+  for (let i = 0; i < str.length; i++) {
+    let iChar = str[i]
+    if (!map.has(iChar)) {
+      map.set(iChar, 1)
+    } else {
+      let count = map.get(iChar)
+      map.set(iChar, count + 1)
+    }
+  }
+  // let arr = [...map].sort((a, b) => b[1] - a[1])
+  //  return arr[0]
+  // 以上是正确答案 以下是我当时写的答案
+
+  return map
+  // 此时得到一个存储了各个字符出现次数的字典
+}
+
+const myStr = 'sdadaqrqgzvzxqweqedas'
+
